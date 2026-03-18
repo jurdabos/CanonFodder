8c25a941059d5ba64da820ee75e463b67489924c
Blai <balazs.torda@iu-study.org>
2026-03-18 11:33:26 +0100

    security: update pyasn1 to 0.6.3 to fix vulnerability

---
+++ tests/integration/conftest.py
@@ -0,0 +1,9 @@
+"""Auto-marks every test under tests/integration/ as integration."""
+import pytest
+
+
+def pytest_collection_modifyitems(items):
+    """Adds the integration marker to all items in this directory tree."""
+    for item in items:
+        if "/integration/" in str(item.fspath):
+            item.add_marker(pytest.mark.integration)
